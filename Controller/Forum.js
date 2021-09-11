659c1fbe4eef687951e9b3edb5b5e0de88becb51
Arivu <thillaiarivu2000@gmail.com>
2021-09-12 04:12:34 +0530

    ForumManagement finished expect Reply Management

--- Controller/Forum.js
+++ Controller/Forum.js
@@ -39,7 +39,7 @@ exports.AddForum = (req, res, next) => {
     .db()
     .collection("Material")
     .insertOne({
-      title: "Forum",
+      title: "TopForum",
       topic: req.body.topic,
       weekID: req.body.weekID,
       weekNo: req.body.weekNo,
@@ -57,5 +57,108 @@ exports.AddForum = (req, res, next) => {
     })
     .catch((er) => {
       console.log(er);
+    });
+};
+
+
+exports.GetTopicForums = (req, res, next) => {
+  // if (req.auth === false) {
+  //   res.status(200).json({ auth: false });
+  //   return;
+  // }
+  db.getDb()
+    .db()
+    .collection("Material")
+    .find({ moduleID:req.query.moduleID, title:"TopForum"})
+    .toArray()
+    .then((resp) => {
+      console.log(resp);
+      res.status(200).json(resp);
+    })
+    .catch(() => {
+      res.status(200).json({ error: true });
+    });
+};
+
+exports.GetTopicForum = (req, res, next) => {
+  // if (req.auth === false) {
+  //   res.status(200).json({ auth: false });
+  //   return;
+  // }
+  
+  db.getDb()
+    .db()
+    .collection("Material")
+    .findOne({ _id: new mongodb.ObjectId(req.query.forumID) })
+    .then((resp) => {
+      console.log(resp);
+      res.status(200).json(resp);
+    })
+    .catch(() => {
+      res.status(200).json({ error: true });
+    });
+};
+
+exports.GetUserName = (req, res, next) => {
+  // if (req.auth === false) {
+  //   res.status(200).json({ auth: false });
+  //   return;
+  // }
+  
+  db.getDb()
+    .db()
+    .collection("User")
+    .findOne({ _id: new mongodb.ObjectId(req.query.userID) })
+    .then((resp) => {
+      console.log(resp);
+      res.status(200).json(resp);
+    })
+    .catch(() => {
+      res.status(200).json({ error: true });
+    });
+};
+
+exports.AddNormalForum = (req, res, next) => {
+  db.getDb()
+    .db()
+    .collection("Material")
+    .insertOne({
+      title: "NormalForum",
+      weekID: req.body.weekID,
+      moduleID: req.body.moduleID,
+      userID: req.body.userID,
+      type: req.body.type,
+      postedDate: getTodayDate(),
+      msg: req.body.msg,
+      replies: [],
+    })
+    .then((resp) => {
+      res.status(200).json(resp);
+      console.log("added");
+    })
+    .catch((er) => {
+      console.log(er);
+    });
+};
+
+
+exports.GetNormalForums = (req, res, next) => {
+  // if (req.auth === false) {
+  //   res.status(200).json({ auth: false });
+  //   return;
+  // }
+  console.log("ijifjisfjisjfijsoj");
+  console.log(req.query);
+  db.getDb()
+    .db()
+    .collection("Material")
+    .find({ moduleID:req.query.moduleID, title:"NormalForum",weekID:req.query.weekID})
+    .toArray()
+    .then((resp) => {
+      console.log(resp);
+      res.status(200).json(resp);
+    })
+    .catch(() => {
+      res.status(200).json({ error: true });
     });
 };
--- routes/ForumManagement.js
+++ routes/ForumManagement.js
@@ -6,4 +6,14 @@ router.get("/get_moduleID/",Forum.GetModuleID);
 
 router.post("/add_forum/",Forum.AddForum);
 
+router.get("/get_topicForums/",Forum.GetTopicForums);
+
+router.get("/get_topicForum/",Forum.GetTopicForum);
+
+router.get("/get_userName/",Forum.GetUserName);
+
+router.post("/add_normalForum/",Forum.AddNormalForum);
+
+router.get("/get_normalForums/",Forum.GetNormalForums);
+
 module.exports = router;
