f0384fec8d75e345fa4aafaf312d9a35596e93e6
Sayanthan <kathirsayanthan@gmail.com>
2021-10-03 18:38:19 +0530

    Enroll.js page token validation

--- Controller/Enroll.js
+++ Controller/Enroll.js
@@ -50,38 +50,38 @@ exports.EnrollStatus = (req, res, next) => {
     });
 };
 
-// exports.GetEnroll = (req, res, next) => {
-//   // if (req.auth === false) {
-//   //   res.status(200).json({ auth: false });
-//   //   return;
-//   // }
-//   db.getDb()
-//     .db()
-//     .collection("Enroll")
-//     .findOne({ id: req.query.id })
-//     .then((resp) => {
-//       if (resp) {
-//         const idArray = resp.students.map((row)=>{
-//           return new mongodb.ObjectId(row)
-//         })
-//         db.getDb()
-//           .db()
-//           .collection("User")
-//           .find({ _id:  {$in:idArray }})
-//           .toArray()
-//           .then((res1) => {
-//             if(res1){
-//               res.status(200).json(res1);
-//             }else{
-//               res.status(200).json({ ack: false });
-//             }
+exports.GetEnroll = (req, res, next) => {
+  if (req.auth === false) {
+    res.status(200).json({ auth: false });
+    return;
+  }
+  db.getDb()
+    .db()
+    .collection("Enroll")
+    .findOne({ id: req.query.id })
+    .then((resp) => {
+      if (resp) {
+        const idArray = resp.students.map((row)=>{
+          return new mongodb.ObjectId(row)
+        })
+        db.getDb()
+          .db()
+          .collection("User")
+          .find({ _id:  {$in:idArray }})
+          .toArray()
+          .then((res1) => {
+            if(res1){
+              res.status(200).json(res1);
+            }else{
+              res.status(200).json({ ack: false });
+            }
             
-//           });
-//       } else {
-//         res.status(200).json({ ack: false });
-//       }
-//     })
-//     .catch((er) => {
-//       res.status(200).json({ ack: false });
-//     });
-// };
+          });
+      } else {
+        res.status(200).json({ ack: false });
+      }
+    })
+    .catch((er) => {
+      res.status(200).json({ ack: false });
+    });
+};
